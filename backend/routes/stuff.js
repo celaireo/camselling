d254b710daf18ea5a4647c25a1db38030260f6b7
younes4638@gmail.com <younes4638@gmail.com>
2023-02-20 13:16:19 +0100

    Ajout des commentaires par younes

--- backend/routes/stuff.js
+++ backend/routes/stuff.js
@@ -5,14 +5,19 @@ const router = express.Router();
 const stuffCtrl = require('../controllers/stuff');
 //const Thing = require('../models/thing');
 
+//Commande pour créer un objet
 router.post('/', stuffCtrl.creatThing);
 
+//Commande pour modifier un objet
 router.put('/:id', stuffCtrl.modifyThing);
 
+//Commande pour afficher un objet
 router.get('/:id', stuffCtrl.getOneThing);
 
+//Commande pour afficher tous les objets 
 router.get('/', stuffCtrl.getAllThings);
 
+//Commande pour supprimer un objet
 router.delete('/:id', stuffCtrl.deleteThing);
   
 
