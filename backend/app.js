2df1c9eca742097c1770460b2fac93388af44d1a
Audrey Maelle <audreymaelle61@gmail.com>
2023-02-20 23:10:09 +0100

    Messages d'alerte

---
+++ backend/app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const stuffRoutes = require('./routes/stuff');
+
+//mongoose.connect('mongodb://localhost:27017/test',
+mongoose.connect('mongodb+srv://opencelio:ocia@cluster0.xb9ucvg.mongodb.net/test?retryWrites=true&w=majority',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+
+const app = express();
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  next();
+});
+
+app.use(bodyParser.json());
+
+app.use('/api/stuff', stuffRoutes);
+
+module.exports = app;
